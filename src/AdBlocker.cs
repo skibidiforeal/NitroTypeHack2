7dae87e24fd9d2f7038e923a2e099b7aabad3fd9
skibidiforeal <ghosted946@gmail.com>
2024-09-13 19:50:16 -0500

    Update AdBlocker.cs

--- src/AdBlocker.cs
+++ src/AdBlocker.cs
@@ -1,5 +1,6 @@
-﻿namespace NitroType3
+﻿namespace NitroType3 
 {
+    //better ad blocker
     class AdBlocker
     {
         private static string[] BlockedDomains = {
@@ -16,6 +17,21 @@
             "pub.network",
             "qualaroo.com",
             "vuukle.com",
+            "googlevideo.com",
+            "youtube.com",
+            "ytimg.com",
+            "googleadservices.com",
+            "gstatic.com",
+            "doubleclick.net"
+        };
+
+        private static string[] BlockedUrls = {
+            "https://marketingmecca.net",
+            "https://cdn.fluidplayer.com",
+            "https://www.googleadservices.com/",
+            "https://www.youtube.com/get_video_info",
+            "https://www.youtube.com/api/stats/ads",
+            "https://www.youtube.com/pagead"
         };
 
         public static bool IsBlocked(string Uri)
@@ -23,6 +39,14 @@
             for (int i = 0; i < BlockedDomains.Length; i++)
             {
                 if (Uri.Contains(BlockedDomains[i]))
+                {
+                    return true;
+                }
+            }
+
+            for (int i = 0; i < BlockedUrls.Length; i++)
+            {
+                if (Uri.Equals(BlockedUrls[i]))
                 {
                     return true;
                 }
